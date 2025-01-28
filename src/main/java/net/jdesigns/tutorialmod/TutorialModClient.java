60a783f1f0f53a333f2e599263eade70d3734fcd
Josh Meyer <jay.designs716@gmail.com>
2025-01-28 14:19:15 -0500

    Initial commit

---
+++ src/main/java/net/jdesigns/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.jdesigns.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
